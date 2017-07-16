f0f77c9a567065b18cf5c0e1f71248d6f32f9017
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-07-16 03:50:34 -0300

    add tictactoe to index.html,add titles for each example

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,14 @@ export default class App extends Component {
     super(props)
   }
   render() {
-    const Cell = ({className,text}) => <span className={className}>{text}</span>
+    const Cell = (props) => {
+      const {className,style} = props
+      return <span {...{className,style}}>{props.text}</span>
+    }
 
     const c = text => () => <Cell text={text} />
 
-    const _ = () => <Cell className="no-shadow" text="&nbsp;" />
+    const _ = () => <Cell className="no-shadow" style={{width:12,border:'1px solid #000'}} />
     const H = c('H')
     const He = c('HE')
     const Li = c('Li')
@@ -22,12 +25,28 @@ export default class App extends Component {
     const C = c('C')
     const B = c('B')
 
+    const X = c('X')
+
     return (
-      <div><Grid matrix={[
-        [H,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,He]
-        ,[Li,Be,_,_,_,_,_,_,_,_,_,_,_,B,C,N,O,F,Ne]
-        ,[H,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,He]
-      ]} /></div>
+      <div>
+        <section>
+          <h2>TicTaeToc</h2>
+          <Grid matrix={[
+            [_,X,_]
+            ,[_,X,_]
+            ,[O,X,O]
+          ]} />
+        </section>
+
+        <section>
+          <h2>Periodic Table</h2>
+          <Grid matrix={[
+            [H,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,He]
+            ,[Li,Be,_,_,_,_,_,_,_,_,_,_,_,B,C,N,O,F,Ne]
+            ,[H,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,He]
+          ]} />
+        </section>
+      </div>
     )
   }
 }
