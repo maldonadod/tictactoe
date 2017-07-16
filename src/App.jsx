869871713d5b22cd99876fe3a685e940f27e27de
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-07-16 02:53:05 -0300

    18 columns,3 rows - periodic table of elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import React, {Component} from 'react'
+import Grid from './components/Grid/Grid'
 
 export default class App extends Component {
   constructor(props) {
     super(props)
   }
   render() {
+    const Cell = ({className,text}) => <span className={className}>{text}</span>
+
+    const c = text => () => <Cell text={text} />
+
+    const _ = () => <Cell className="no-shadow" text="&nbsp;" />
+    const H = c('H')
+    const He = c('HE')
+    const Li = c('Li')
+    const Be = c('Be')
+    const Ne = c('Ne')
+    const F = c('F')
+    const O = c('O')
+    const N = c('N')
+    const C = c('C')
+    const B = c('B')
+
     return (
-      <div>Hello World</div>
+      <div><Grid matrix={[
+        [H,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,He]
+        ,[Li,Be,_,_,_,_,_,_,_,_,_,_,_,B,C,N,O,F,Ne]
+        ,[H,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,He]
+      ]} /></div>
     )
   }
 }
