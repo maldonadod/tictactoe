0fc669b7a9d24f78b09f0d07436aec7f10c51b87
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-07-28 01:38:27 -0300

    add socket.io,add some player and matrix logic

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const express = require('express');
-const app = express();
 const PORT = process.env.PORT || 8900;
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -9,7 +11,41 @@ app.get('/', function(req, res) {
   res.sendFile(`${__dirname}/public/index.html`);
 });
 
-app.listen(PORT, error => {
+let players = []
+let matrix = [['','',''],['','',''],['','','']]
+
+io.on('connection', socket => {
+
+  socket.emit('matrix:state', {matrix})
+
+  socket.on('player:register', user => {
+
+    players.push({
+      socketId: socket.id,
+      name: user.name,
+      type: players.length === 0 ? 'X' : 'O'
+    })
+  })
+
+  socket.on('player:move', ({row,cell}) => {
+
+    const p = players.filter(p => p.socketId === socket.id).pop()
+    const type = p.type
+
+    matrix = matrix.map((row_cells,row_index) => {
+      return row_cells.map((cell_text, cell_index) => {
+        if (row_index === row && cell_index === cell) {
+          return type
+        }
+        return cell_text
+      })
+    })
+
+    io.sockets.emit('matrix:state', {matrix})
+  })
+})
+
+server.listen(PORT, error => {
   error
   ? console.error(error)
   : console.info(`Listening on port ${PORT}.`)
