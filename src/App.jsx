512fba12487fdfc97830dc453fffc5f3a19e328a
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-07-28 01:32:50 -0300

    map playerName to dispatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
 import React, {Component} from 'react'
 import Tictactoe from './components/Tictactoe'
+import { connect } from 'react-redux'
 
-export default class App extends Component {
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.register = this.register.bind(this)
+  }
+  register(e) {
+    if(e.keyCode == 13) {
+      const name = e.target.value;
+      this.props.playerName({name})
+    }
+  }
   render() {
     return (
       <div>
         <Tictactoe />
+        <input name='player_name' type='text' onKeyDown={this.register} />
       </div>
     )
   }
 }
+
+const mapDispatchToProps = dispatch => {
+  return {
+    playerName: player => {
+      return dispatch({
+        type: 'PLAYER_NAME',
+        player
+      })
+    }
+  }
+}
+
+export default connect(null, mapDispatchToProps)(App)
