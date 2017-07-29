7bdc589d8a6ddbefdcecbe96f56f0370cd85a3c7
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-07-29 00:28:18 -0300

    fix on Grid and Row tests

--- src/components/Grid/Grid.spec.js
+++ src/components/Grid/Grid.spec.js
@@ -1,15 +1,25 @@
 import React from 'react'
+import Cell from '../Cell'
 import Grid from './Grid'
-import Column from '../Column/Column'
-import {shallow} from 'enzyme'
+import Row from '../Row/Row'
+import {shallow,mount} from 'enzyme'
 
 describe('<Grid />', () => {
 
   var factoryWrapper;
-  const Cell = () => (<a>****</a>)
-
   beforeAll(() => {
-    factoryWrapper = props => <Grid {...props} />
+    const children = matrix => () => (
+      matrix ? matrix.map((c,row) => (
+        <Row key={row}>
+          {() => c.map((text,i) => <Cell text={text} key={i} />)}
+        </Row>
+      )) : ''
+    )
+    factoryWrapper = props => (
+      <Grid>
+        {children(props.matrix)}
+      </Grid>
+    )
   })
 
   it('is a div wrapper', () => {
@@ -17,24 +27,24 @@ describe('<Grid />', () => {
     expect(wrapper.type()).toBe('div')
   })
 
-  it('should nest as many children as columns passed in', () => {
+  it('should nest as many children as rows passed in', () => {
     {
       const wrapper = shallow(factoryWrapper({}))
       expect(wrapper.children().length).toEqual(0);
     }
     {
-      const matrix = [[Cell]]
+      const matrix = [['']]
       const wrapper = shallow(factoryWrapper({matrix}))
-      expect(wrapper.children().at(0).type()).toEqual(Column);
+      expect(wrapper.children().at(0).type()).toEqual(Row);
       expect(wrapper.children().at(1).type()).toEqual(null);
       expect(wrapper.children().length).toEqual(1);
     }
     {
       const matrix = [[Cell, Cell, Cell],[Cell, Cell], [Cell]]
       const wrapper = shallow(factoryWrapper({matrix}))
-      expect(wrapper.children().at(0).type()).toEqual(Column);
-      expect(wrapper.children().at(1).type()).toEqual(Column);
-      expect(wrapper.children().at(2).type()).toEqual(Column);
+      expect(wrapper.children().at(0).type()).toEqual(Row);
+      expect(wrapper.children().at(1).type()).toEqual(Row);
+      expect(wrapper.children().at(2).type()).toEqual(Row);
       expect(wrapper.children().at(4).type()).toEqual(null);
       expect(wrapper.children().length).toEqual(3);
     }
