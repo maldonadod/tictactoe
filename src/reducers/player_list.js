00dc70abd218096c093240fe2a207672b3f6f273
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-08-05 10:51:34 -0300

    import and use action instead of hardcode it

--- src/reducers/player_list.js
+++ src/reducers/player_list.js
@@ -1,6 +1,8 @@
+import {SET_PLAYER_LIST} from '../actions'
+
 export default function player_list(state = [], {type,players}) {
   switch (type) {
-    case 'SET_PLAYER_LIST':
+    case SET_PLAYER_LIST:
       return [...players]
       break;
     default:
