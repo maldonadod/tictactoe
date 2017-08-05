01c0a136930157644d3d717a9c9e85ed4288fc32
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-08-05 10:42:37 -0300

    actions index just exports actions members

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,3 @@
-const MATRIX_STATE = 'MATRIX_STATE'
-const SET_PLAYER_LIST = 'SET_PLAYER_LIST'
-
-const updateMatrix = matrix => ({
-  type: MATRIX_STATE,
-  matrix
-})
-const updatePlayerList = players => ({
-  type: 'SET_PLAYER_LIST',
-  players
-})
-
-export {
-  MATRIX_STATE
-  ,SET_PLAYER_LIST
-  ,updateMatrix
-  ,updatePlayerList
-}
+export * from './matrix'
+export * from './players'
+export * from './game'
