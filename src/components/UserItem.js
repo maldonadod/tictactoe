d6d639dccb00f647f7adcf8cf63fd625d5d48447
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-08-05 10:49:14 -0300

    add UserItem, it handles onClick game request

--- src/components/UserItem.js
+++ src/components/UserItem.js
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {gameRequest} from '../actions'
 
-const UserItem = ({user}) => (
-  <p>{user.name}</p>
-)
-export default UserItem
+class UserItem extends Component {
+  constructor(props) {
+    super(props)
+    this.onClick = this.onClick.bind(this)
+  }
+  onClick() {
+    this.props.gameRequest([this.props.user, this.props.player])
+  }
+  render() {
+    return (
+      <li onClick={this.onClick}>{this.props.user.name}</li>
+    )
+  }
+}
+
+const mapState = state => {
+  return {
+    player: state.player
+  }
+}
+const mapDispatch = dispatch => {
+  return {
+    gameRequest: players => dispatch(gameRequest(players))
+  }
+}
+
+export default connect(mapState,mapDispatch)(UserItem)
