c1cc4b88b1b5e6bc6d18b754a0151414e84541e4
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-08-05 11:34:29 -0300

    isolate form for player register into WhoAreYou component

---
+++ src/components/WhoAreYou.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react'
+import {Input, Header} from 'semantic-ui-react'
+
+class WhoAreYou extends Component {
+  constructor(props) {
+    super(props)
+    this.register = this.register.bind(this)
+  }
+  register() {
+    const {onRegister} = this.props
+    if(e.keyCode == 13) {
+      onRegister({
+        name: e.target.value
+      })
+    }
+  }
+  render () {
+    return (
+      <div className="player-register">
+         <Header>Who are you ?</Header>
+         <Input
+           type="text"
+           autoFocus
+           transparent
+           placeholder="Your Name"
+           onKeyDown={this.register} />
+       </div>
+    )
+  }
+}
+
+export default WhoAreYou
