576dc1e780f796d40aa4b44f561286a543fa4108
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-08-06 13:33:21 -0300

    game_request reducer: init state to {} and reset state on request resolution

--- src/reducers/game_request.js
+++ src/reducers/game_request.js
@@ -4,16 +4,16 @@ import {
   ,REJECT_GAME_REQUEST
 } from '../actions'
 
-export default function game_request(state = null, {type,request}) {
+export default function game_request(state = {}, {type,request}) {
   switch (type) {
       case SHOW_CLIENT_GAME_REQUEST:
       return {...request}
     break;
       case ACCEPT_GAME_REQUEST:
-      return state
+      return {}
     break;
       case REJECT_GAME_REQUEST:
-      return state
+      return {}
     break;
     default:
       return state
