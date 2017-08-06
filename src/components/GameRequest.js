f7145b203c1d6435795c2af198a7acc260ef4704
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-08-06 13:32:21 -0300

    GameReques: remove all open/close modal logic

--- src/components/GameRequest.js
+++ src/components/GameRequest.js
@@ -4,38 +4,22 @@ import { Button, Header, Icon, Modal } from 'semantic-ui-react'
 import {acceptGameRequest,rejectGameRequest} from '../actions'
 
 class GameRequest extends Component {
-
   constructor(props) {
     super(props)
     this.reject = this.reject.bind(this)
     this.accept = this.accept.bind(this)
-    this.state = {
-      open: true
-    }
   }
-
-  closeModal() {
-    this.setState(state => {
-      return {...state, open: false}
-    })
-  }
-
   reject() {
-    this.closeModal()
-    this.props.reject([this.props.player, this.props.request.opponent])
+    this.props.reject(this.props.opponent)
 
   }
-
   accept() {
-    this.closeModal()
     this.props.accept(this.props.opponent)
   }
-
   render() {
     const {opponent} = this.props
-    const {open} = this.state
     return (
-      <Modal open={open} basic size='small'>
+      <Modal open basic size='small'>
         <Header icon='archive' content='Game Request' />
         <Modal.Content>
           <p>{opponent.name} wants to play, accept ?</p>
