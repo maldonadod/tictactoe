eb79dcadad182892bc68e1413a44a1e15a5a21df
Daniel Maldonado <emaldonado.daniel@gmail.com>
2017-08-05 23:13:46 -0300

    add game request actions

--- src/actions/game.js
+++ src/actions/game.js
@@ -1,13 +1,30 @@
 const GAME_REQUEST = 'GAME_REQUEST'
 const SHOW_CLIENT_GAME_REQUEST = 'SHOW_CLIENT_GAME_REQUEST'
+const ACCEPT_GAME_REQUEST = 'ACCEPT_GAME_REQUEST'
+const REJECT_GAME_REQUEST = 'REJECT_GAME_REQUEST'
+const GAME_START = 'GAME_START'
 
-const gameRequest = players => ({
+const gameRequest = user => ({
   type: GAME_REQUEST,
-  players
+  user
 })
-const showClientGameRequest = game_request => ({
+const showClientGameRequest = opponent => ({
   type: SHOW_CLIENT_GAME_REQUEST,
+  request: {
+    opponent
+  }
+})
+const acceptGameRequest = player => ({
+  type: ACCEPT_GAME_REQUEST,
+  player
+})
+const rejectGameRequest = game_request => ({
+  type: REJECT_GAME_REQUEST,
   game_request
+})
+const handleGameStart = game => ({
+  type: GAME_START,
+  game
 })
 
 export {
@@ -15,4 +32,10 @@ export {
   ,gameRequest
   ,SHOW_CLIENT_GAME_REQUEST
   ,showClientGameRequest
+  ,ACCEPT_GAME_REQUEST
+  ,acceptGameRequest
+  ,REJECT_GAME_REQUEST
+  ,rejectGameRequest
+  ,GAME_START
+  ,handleGameStart
 }
